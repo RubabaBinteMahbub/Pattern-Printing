68d33df61f381084a12e6daf1fa51c781619167a
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-04-25 13:06:11 +0600

    Create Pattern - 5.c for number pattern display
    Add a C program to print a specific number pattern.

---
+++ Pattern - 5.c
@@ -0,0 +1,21 @@
+/*
+  1
+ 21
+321
+*/
+#include<stdio.h>
+int main()
+{
+    int n=3,i,j;
+    printf("n = %d\n",n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+        printf(" ");
+        }
+        for(j=i;j>=1;j--){
+        printf("%d",j);
+        }
+    printf("\n");
+    }
+return 0;
+}
