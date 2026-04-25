725a5b32d047aff0f5fe84ab83b69104eebe4d52
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-04-25 13:04:06 +0600

    Add pattern printing program in Pattern - 4.c
    Implement a pattern printing program that takes an integer input and prints a specific pattern of numbers.

---
+++ Pattern - 4.c
@@ -0,0 +1,26 @@
+/*
+  1
+ 12
+123
+*/
+#include<stdio.h>
+int main()
+{
+  int n,i,j;
+  printf("n = ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    for(j=1;j<=n-i;j++)
+    {
+        printf(" ");
+    }
+    for(j=1;j<=i;j++)
+    {
+        printf("%d",j);
+    }
+     
+    printf("\n");
+  }
+return 0;
+}
